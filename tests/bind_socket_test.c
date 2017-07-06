6364c8678048533de4b70df24d7841c022a774f9
Zack Brown <zbrown@pillartechnology.com>
2017-07-06 11:51:57 -0400

    more tests for bind socket

--- tests/bind_socket_test.c
+++ tests/bind_socket_test.c
@@ -6,19 +6,35 @@
 #include "../src/bind_socket.h"
 #include "bind_socket_test.h"
 
+SIMULACRUM(void, bind, 3, int, struct sockaddr *, size_t)
+struct bind_args_s
+{
+    int socket;
+    struct sockaddr_in *address;
+    size_t address_size;
+};
+
+struct bind_args_s captured_args;
+
+static void bind_callback(int socket, struct sockaddr_in *address, size_t size)
+{
+    captured_args.socket = socket;
+    captured_args.address = address;
+    captured_args.address_size = size;
+}
+
 static void setup()
 {
-
 }
 
 static void teardown()
 {
-
+    mock_reset_call_count(&bind_mock);
 }
 
 START_TEST(it_sets_socket_internet_family_address_and_port)
 {
-    // Arrange 
+    // Arrange
     struct sockaddr_in address = {0};
 
     // convert port to network short (Big Endian)
@@ -28,10 +44,29 @@ START_TEST(it_sets_socket_internet_family_address_and_port)
     bind_socket(&address, 0, 80);
 
     // Assert
-    
+
     ck_assert_int_eq(address.sin_family, AF_INET);
     ck_assert_int_eq(address.sin_addr.s_addr, INADDR_ANY);
     ck_assert_int_eq(address.sin_port, expected_port);
+}
+END_TEST
+
+START_TEST(it_binds_the_socket)
+{
+    // Arrange
+    struct sockaddr_in address = {0};
+    int expected_socket = 4;
+
+    mock_set_callback(&bind_mock, &bind_callback);
+
+    // Act
+    bind_socket(&address, expected_socket, 80);
+
+    // Assert
+    ck_assert_int_eq(mock_get_call_count(&bind_mock), 1);
+    ck_assert_int_eq(captured_args.socket, expected_socket);
+    ck_assert_ptr_eq(captured_args.address, &address);
+    ck_assert_int_eq(captured_args.address_size, sizeof(address));
 }
 END_TEST
 
@@ -46,6 +81,7 @@ Suite *make_bind_socket_test_suite(void)
     tcase_add_checked_fixture(tc, &setup, &teardown);
 
     tcase_add_test(tc, it_sets_socket_internet_family_address_and_port);
+    tcase_add_test(tc, it_binds_the_socket);
     suite_add_tcase(s, tc);
 
     return s;

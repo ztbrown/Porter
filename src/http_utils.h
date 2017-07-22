1225d15ff85ed262af609304bb28b503c7b6e002
Zack Brown <zbrown@pillartechnology.com>
2017-07-21 21:48:30 -0400

    tests for send http header

--- src/http_utils.h
+++ src/http_utils.h
@@ -12,5 +12,6 @@ void handle(int connecting_socket);
 int getRequestType(char *input);
 int handleHttpGET(char *input, int connecting_socket);
 int send_string(char *message, int socket);
+void send_header(char *status_code, char *content_Type, int total_size, int socket);
 
 #endif // HTTP_UTILS_H
